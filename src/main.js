8e0ffda1f46486f9997e1d9914ef7594ba1c13d4
mayukoooo <yamagishi.work3@gmail.com>
2022-01-23 14:42:17 +0900

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import './assets/main.css'
 
-createApp(App).mount('#app')
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faHeart } from "@fortawesome/free-solid-svg-icons"
+
+library.add(faHeart)
+createApp(App).use(router).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
